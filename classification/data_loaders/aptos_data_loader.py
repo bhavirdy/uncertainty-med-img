fa893dbdff45c9dc3f0676aecce5116c690927a5
Bhavishyan Reddy <bhavirdy@gmail.com>
2025-09-27 19:35:07 +0200

    add data augmentation

--- classification/data_loaders/aptos_data_loader.py
+++ classification/data_loaders/aptos_data_loader.py
@@ -94,9 +94,28 @@ def get_aptos_loaders(train_csv="./classification/datasets/aptos2019/train_split
                              std=[0.229, 0.224, 0.225])
     ])
 
-    train_set = APTOSDataset(train_csv, img_dir, transform=tf, circle_crop=circle_crop)
-    val_set = APTOSDataset(val_csv, img_dir, transform=tf, circle_crop=circle_crop)
-    test_set = APTOSDataset(test_csv, img_dir, transform=tf, circle_crop=circle_crop)
+    train_tf = transforms.Compose([
+        transforms.Resize((input_size, input_size)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomVerticalFlip(p=0.5),
+        transforms.RandomRotation(degrees=15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.05),
+        transforms.RandomResizedCrop(input_size, scale=(0.9, 1.0)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
+    ])
+
+    val_tf = transforms.Compose([
+        transforms.Resize((input_size, input_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
+    ])
+
+    train_set = APTOSDataset(train_csv, img_dir, transform=train_tf, circle_crop=circle_crop)
+    val_set = APTOSDataset(val_csv, img_dir, transform=val_tf, circle_crop=circle_crop)
+    test_set = APTOSDataset(test_csv, img_dir, transform=val_tf, circle_crop=circle_crop)
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
