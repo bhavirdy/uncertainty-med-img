a0fd6410136d8af260f163b1f088a8cd0286fbd9
Bhavishyan Reddy <bhavirdy@gmail.com>
2025-10-09 23:03:34 +0200

    Add dropout to ResNet50EDL final layer

--- classification/models/resnet_edl.py
+++ classification/models/resnet_edl.py
@@ -3,21 +3,17 @@ import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
 
 class ResNet50EDL(nn.Module):
-    def __init__(self, num_classes, weights=ResNet50_Weights.DEFAULT):
+    def __init__(self, num_classes, weights=ResNet50_Weights.DEFAULT, dropout_p=0.5):
         super().__init__()
         self.model = resnet50(weights=weights)
-
-        # Freeze backbone
         for param in self.model.parameters():
             param.requires_grad = False
 
-        # Replace final FC layer
         self.model.fc = nn.Sequential(
+            nn.Dropout(p=dropout_p),
             nn.Linear(self.model.fc.in_features, num_classes),
-            nn.Softplus()  # ensures non-negative evidence outputs
+            nn.Softplus() # ensures non-negative evidence outputs
         )
-
-        # Train FC layer
         for param in self.model.fc.parameters():
             param.requires_grad = True
 
