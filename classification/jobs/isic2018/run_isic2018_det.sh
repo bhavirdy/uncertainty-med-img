e0a07e3c33d21961cbdc988f3706a0d382c8fa55
Bhavishyan Reddy <bhavirdy@gmail.com>
2025-11-09 22:54:11 +0200

    Increase training epochs to 30 for ISIC2018 jobs

--- classification/jobs/isic2018/run_isic2018_det.sh
+++ classification/jobs/isic2018/run_isic2018_det.sh
@@ -21,7 +21,7 @@ python -m classification.scripts.train \
     --dataset isic2018 \
     --num_classes 7 \
     --output_dir "$TRAIN_DIR" \
-    --epochs 10 \
+    --epochs 30 \
     --batch_size 32 \
     --num_workers 8 \
     --lr 1e-4 \
