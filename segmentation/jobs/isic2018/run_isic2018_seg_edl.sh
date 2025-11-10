4719a42bd210f6a637e2f768bf89ef733a885590
Bhavishyan Reddy <bhavirdy@gmail.com>
2025-11-10 22:43:23 +0200

    Update run_isic2018_seg_edl.sh

--- segmentation/jobs/isic2018/run_isic2018_seg_edl.sh
+++ segmentation/jobs/isic2018/run_isic2018_seg_edl.sh
@@ -22,7 +22,7 @@ python -m segmentation.scripts.train \
     --dataset isic2018 \
     --num_classes 2 \
     --output_dir "$TRAIN_DIR" \
-    --epochs 50 \
+    --epochs 10 \
     --batch_size 32 \
     --num_workers 8 \
     --lr 1e-4 \
