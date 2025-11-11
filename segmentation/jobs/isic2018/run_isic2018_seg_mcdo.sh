43071f60dd06f239b265ba860ffe152e4223a0b9
Bhavishyan Reddy <bhavirdy@gmail.com>
2025-11-11 16:37:57 +0200

    Avoid cluster node 75

--- segmentation/jobs/isic2018/run_isic2018_seg_mcdo.sh
+++ segmentation/jobs/isic2018/run_isic2018_seg_mcdo.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=24:00:00
-#SBATCH --exclude=mscluster42,mscluster44,mscluster48,mscluster65
+#SBATCH --exclude=mscluster42,mscluster44,mscluster48,mscluster65,mscluster75
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate opencv_env
