03dc6bbe283c07c0e4b156bae9fb02c6aa2e0f90
Bhavishyan Reddy <bhavirdy@gmail.com>
2025-11-11 15:38:21 +0200

    Configure epochs and early stop patience

--- segmentation/jobs/isic2018/run_isic2018_seg_mcdo.sh
+++ segmentation/jobs/isic2018/run_isic2018_seg_mcdo.sh
@@ -22,11 +22,11 @@ python -m segmentation.scripts.train \
     --dataset isic2018 \
     --num_classes 2 \
     --output_dir "$TRAIN_DIR" \
-    --epochs 10 \
+    --epochs 50 \
     --batch_size 32 \
     --num_workers 8 \
     --lr 1e-4 \
-    --early_stop_patience 10 \
+    --early_stop_patience 20 \
     --method mcdo \
     --dropout 0.3
 
